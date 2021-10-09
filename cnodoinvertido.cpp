a5209f695b83c5f04ff406839fe7feb0206cfd52
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-09 00:37:11 -0500

    Definicion e implementacion de la clase cNodoInverso

--- cnodoinvertido.cpp
+++ cnodoinvertido.cpp
@@ -1,6 +1,30 @@
 #include "cnodoinvertido.h"
 
-cNodoInvertido::cNodoInvertido()
-{
+cNodoInvertido::cNodoInvertido() {}
 
+cNodoInvertido::cNodoInvertido(int ai) {
+    valorAI = ai;
+    pAnt = nullptr;
+    pSig = nullptr;
+}
+
+void cNodoInvertido::insertarAdelante(int ai, cObjeto* obj) {
+    cNodoInvertido* newNode = new cNodoInvertido(ai);
+    newNode->lst.insertarAlFinal(obj);
+
+    newNode->pSig = this->pSig;
+    newNode->pAnt = this;
+    this->pSig = newNode;
+    this->pSig->pSig->pAnt = newNode;
+}
+
+int cNodoInvertido::eliminarAdelante() {
+    int ai;
+    ai = this->pSig->valorAI;
+
+    this->pSig = this->pSig->pSig;
+    delete this->pSig->pAnt;
+    this->pSig->pAnt = this;
+
+    return ai;
 }
--- cnodoinvertido.h
+++ cnodoinvertido.h
@@ -1,11 +1,23 @@
 #ifndef CNODOINVERTIDO_H
 #define CNODOINVERTIDO_H
 
+#include "clista.h"
+#include "cobjeto.h"
 
-class cNodoInvertido
-{
+class cNodoInvertido {
+private:
+    int valorAI; // valorAtributoInvertido
+    cLista lst;
+
+    cNodoInvertido* pAnt;
+    cNodoInvertido* pSig;
+
 public:
     cNodoInvertido();
+    cNodoInvertido(int ai);
+
+    void insertarAdelante(int edad, cObjeto* obj);
+    int eliminarAdelante();
 };
 
 #endif // CNODOINVERTIDO_H
