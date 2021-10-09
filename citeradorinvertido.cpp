4d3ce0b303565fb7a375b092b6afdcd09bc995ad
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-09 17:45:58 -0500

    Definicion e implementacion de la clase cIteradorInverso

--- citeradorinvertido.cpp
+++ citeradorinvertido.cpp
@@ -1,6 +1,55 @@
 #include "citeradorinvertido.h"
 
-cIteradorInvertido::cIteradorInvertido()
-{
+cIteradorInvertido::cIteradorInvertido() {}
 
+void cIteradorInvertido::insertarNodoInvertido(int ai, cObjeto *obj) {
+    pActual->insertarNodoInvertido(ai, obj);
+}
+
+int cIteradorInvertido::eliminarNodoInvertido() {
+    return pActual->eliminarNodoInvertido();
+}
+
+void cIteradorInvertido::insertarElemento(cObjeto *obj) {
+    pActual->insertarElemento(obj);
+}
+
+cObjeto *cIteradorInvertido::eliminarElemento() {
+    return pActual->eliminarElemento();
+}
+
+
+    // Sobrecarga de operadores
+void cIteradorInvertido::operator =(cIteradorInvertido &otro) {
+    pActual = otro.pActual;
+}
+
+bool cIteradorInvertido::operator ==(cIteradorInvertido &otro) {
+    return (pActual == otro.pActual);
+}
+
+bool cIteradorInvertido::operator !=(cIteradorInvertido &otro) {
+    return (pActual != otro.pActual);
+}
+
+    // Prefijos
+void cIteradorInvertido::operator ++() {
+    pActual = pActual->pSig;
+}
+
+void cIteradorInvertido::operator --() {
+    pActual = pActual->pAnt;
+}
+
+int cIteradorInvertido::operator *() {
+    return pActual->valorAI;
+}
+
+    //Postfijos
+void cIteradorInvertido::operator ++(int) {
+    pActual = pActual->pSig;
+}
+
+void cIteradorInvertido::operator --(int) {
+    pActual = pActual->pAnt;
 }
--- citeradorinvertido.h
+++ citeradorinvertido.h
@@ -1,11 +1,42 @@
 #ifndef CITERADORINVERTIDO_H
 #define CITERADORINVERTIDO_H
 
+#include "cnodoinvertido.h"
 
-class cIteradorInvertido
-{
+class cIteradorInvertido {
+private:
+    cNodoInvertido* pActual;
+
+    void setNodo(cNodoInvertido* pNodo);
+
+    friend class cListaInvertida;
+
 public:
     cIteradorInvertido();
+
+    // Insertar y eliminar en la lista invertida
+    void insertarNodoInvertido(int ai, cObjeto* obj);
+    int eliminarNodoInvertido(); // Retorna el valor del atributo invertido
+
+    void insertarElemento(cObjeto* obj);
+    cObjeto* eliminarElemento();
+
+
+    // Sobrecarda de operadores
+    void operator =(cIteradorInvertido& otro);
+
+    bool operator ==(cIteradorInvertido& otro);
+    bool operator !=(cIteradorInvertido& otro);
+
+    // Prefijos
+    void operator ++();
+    void operator --();
+
+    int operator *();
+
+    // Postfijos
+    void operator ++(int);
+    void operator --(int);
 };
 
 #endif // CITERADORINVERTIDO_H
