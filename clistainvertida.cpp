c77c7a0c878d58e55792fce2c7c17affa6a5584e
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-10 13:11:01 -0500

    Definicion e implementacion de la lista invertida

--- clistainvertida.cpp
+++ clistainvertida.cpp
@@ -1,6 +1,113 @@
 #include "clistainvertida.h"
 
-cListaInvertida::cListaInvertida()
-{
+cListaInvertida::cListaInvertida() {
+    inicio.pSig = &final;
+    final.pAnt =&inicio;
+}
 
+cListaInvertida::~cListaInvertida() {
+    cIteradorInvertido it;
+
+    for (it=this->begin(); it!=this->end(); it++) {
+        inicio.pSig = inicio.pSig->pSig;
+
+        it.pActual->lst.~cLista();
+        delete it.pActual;
+    }
+
+    final.pAnt = &inicio;
+}
+
+
+void cListaInvertida::insertarElemento(int ai, cObjeto *obj) {
+    cIteradorInvertido it;
+
+    for(it=this->begin(); it!=this->end(); it++)
+        if (it.pActual->valorAI == ai) {
+            it.pActual->insertarElemento(obj);
+            return;
+        }
+
+    final.pAnt->insertarNodoInvertido(ai, obj);
+}
+
+int cListaInvertida::eliminarNodo() { // De la lista invertida
+    if(isEmpty())
+        return -1;
+    return final.pAnt->pAnt->eliminarNodoInvertido();
+}
+
+void cListaInvertida::busqueda(int ai) {
+    cIteradorInvertido it;
+
+    for(it=this->begin(); it!=this->end(); it++)
+        if(it.pActual->valorAI == ai) {
+            it.pActual->lst.imprimir();
+            return;
+        }
+    std::cout << "Lista no encontrada" << std::endl;
+}
+
+int cListaInvertida::size() {
+    int numNodes = 0;
+    cIteradorInvertido it;
+
+    for (it=this->begin(); it!=this->end(); it++)
+        numNodes++;
+
+    return numNodes;
+}
+
+bool cListaInvertida::isEmpty() {
+    return inicio.pSig == &final;
+}
+
+bool cListaInvertida::isFull() {
+    return false;
+}
+
+void cListaInvertida::imprimir() {
+    cIteradorInvertido it;
+
+    for (it=this->begin(); it!=this->end(); it++) {
+        it.pActual->lst.imprimir();
+        std::cout << std::endl << std::endl;
+    }
+}
+
+cIteradorInvertido &cListaInvertida::begin() {
+    itBegin.setNodo(inicio.pSig);
+    return itBegin;
+}
+
+cIteradorInvertido &cListaInvertida::end() {
+    itEnd.setNodo(final.pAnt);
+    return itEnd;
+}
+
+void cListaInvertida::Salvar(std::fstream &out) {
+    out << GetCLSID() << std::endl;
+    out << size() << std::endl;
+
+    cIteradorInvertido it;
+
+    for (it=this->begin(); it!=this->end(); it++) {
+        it.pActual->lst.Salvar(out);
+    }
+}
+
+void cListaInvertida::Cargar(std::fstream &in) {
+    int lstInvertSize;
+    in >> lstInvertSize;
+    cIteradorInvertido it;
+
+    for (it=this->begin(); it!=this->end(); it++) {
+        int lstId;
+        in >> lstId;
+        it.pActual->lst.Cargar(in);
+    }
+}
+
+int cListaInvertida::GetCLSID() {
+    return CLSID_LISTAINVERTIDA;
 }
--- clistainvertida.h
+++ clistainvertida.h
@@ -1,11 +1,48 @@
 #ifndef CLISTAINVERTIDA_H
 #define CLISTAINVERTIDA_H
 
+#include <iostream>
 
-class cListaInvertida
-{
+#include "cnodoinvertido.h"
+#include "citeradorinvertido.h"
+
+#include "clista.h"
+#include "calumno.h"
+
+#define CLSID_LISTAINVERTIDA 2000
+
+class cListaInvertida : public cObjeto {
+private:
+    cNodoInvertido inicio;
+    cNodoInvertido final;
+
+    cIteradorInvertido itBegin;
+    cIteradorInvertido itEnd;
+
+
 public:
     cListaInvertida();
+    ~cListaInvertida() override;
+
+    void insertarElemento(int ai, cObjeto* obj);
+
+    int eliminarNodo(); // De la lista invertida
+
+    void busqueda(int ai);
+
+    int size();
+
+    bool isEmpty();
+    bool isFull();
+
+    void imprimir() override;
+
+    cIteradorInvertido& begin();
+    cIteradorInvertido& end();
+
+    void Salvar(std::fstream& out) override;
+    void Cargar(std::fstream& in) override;
+    int GetCLSID() override;
 };
 
 #endif // CLISTAINVERTIDA_H
